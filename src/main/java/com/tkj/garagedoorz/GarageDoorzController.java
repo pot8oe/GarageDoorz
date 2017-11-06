a1adf185ca2c0467084d5f9bf18af39273ad6ef5
Thomas G. Kenny Jr <tom8oe@gmail.com>
2017-11-06 11:55:20 -0500

    Cleaned up imports.

--- src/main/java/com/tkj/garagedoorz/GarageDoorzController.java
+++ src/main/java/com/tkj/garagedoorz/GarageDoorzController.java
@@ -4,12 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
-
 /**
  * REST controler that maps REST endpoints to hardware control.
  * @author Thomas G. Kenny Jr
