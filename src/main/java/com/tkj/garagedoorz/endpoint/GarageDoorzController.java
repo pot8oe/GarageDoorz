ef6ba49391316e7a0afdd165239a8577e228bc8b
Thomas G. Kenny Jr <tom8oe@gmail.com>
2017-11-08 14:39:52 -0500

    Cleaned up merge of feature/refactor_configuration

--- src/main/java/com/tkj/garagedoorz/endpoint/GarageDoorzController.java
+++ src/main/java/com/tkj/garagedoorz/endpoint/GarageDoorzController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * REST controler that maps REST endpoints to hardware control.
+ * Controller that maps REST end points to Garage door hardware status and control.
  * @author Thomas G. Kenny Jr
  *
  */
@@ -26,43 +26,79 @@ public class GarageDoorzController {
 
 	}
 
+	/**
+	 * @return Hello Doorz
+	 */
 	@RequestMapping ("/" )
 	public String HelloDoorz() {
 
 		return "Hello Doorz";
 	}
 	
+	/**
+	 * @return The status of the Garage.
+	 */
 	@RequestMapping( "/getGarageStatus" )
 	public List<DoorStatus> getGarageStatus() {
 
 		return hwController.getGarageDoorStatuses();
 	}
 	
+	/**
+	 * Return if the door is open. At present we do not have any in motion state information.
+	 * @param door Index of door to read open state. Defaults to 0 (first door).
+	 * @return True if door is not fully closed.
+	 */
 	@RequestMapping( "/{door}/isDoorOpen" )
 	public boolean isDoorOpen( @PathVariable int door ) {
 		
 		return hwController.isDoorOpen( door );
 	}
 	
-	@RequestMapping( "/{door}/toggleDoor" )
+	/**
+	 * Press the requested garage door button.
+	 * @param door Index of door to actuate. Defaults to 0 (first door).
+	 */
+	@RequestMapping( "/{door}/pressDoorButton" )
 	@ResponseStatus( HttpStatus.ACCEPTED )
-	public void toggleDoor( @PathVariable int door ) {
+	public void pressDoorButton( @PathVariable int door ) {
 
 		hwController.pressDoorButton( door );
 
 	}
 	
+	/**
+	 * Opens the requested door if isOpen reports false. This function can erroneously stop the door if it
+	 * is in motion when called. Door isOpen reports true as soon as the door is not closed and the
+	 * state of motion is unknown at this time.
+	 * @param door Index of door to actuate. Defaults to 0 (first door).
+	 * @return True if presssDoorButton is called
+	 */
 	@RequestMapping( "/{door}/openDoor" )
 	@ResponseStatus( HttpStatus.ACCEPTED )
-	public void openDoor( @PathVariable int door ) {
-
-
+	public boolean openDoor( @PathVariable int door ) {
+		if(!hwController.isDoorOpen( door )) {
+			hwController.pressDoorButton( door );
+			return true;
+		}
+		return false;
 	}
 
+	/**
+	 * Closes the requested door if isOpen reports true. This function can erroneously stop the door if it
+	 * is in motion when called. Door isOpen reports true as soon as the door is not closed and the
+	 * state of motion is unknown at this time.
+	 * @param door Index of door to actuate. Defaults to 0 (first door).
+	 * @return
+	 */
 	@RequestMapping( "/{door}/closeDoor" )
 	@ResponseStatus( HttpStatus.ACCEPTED )
-	public void closeDoor( @PathVariable int door ) {
-
+	public boolean closeDoor( @PathVariable int door ) {
+		if(hwController.isDoorOpen( door )) {
+			hwController.pressDoorButton( door );
+			return true;
+		}
+		return false;
 	}
 
 }
