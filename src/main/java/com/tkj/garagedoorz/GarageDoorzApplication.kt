fb5ff2afabe3edf52cb8532c4943036b95860fac
Daniel Frey <dmfrey@gmail.com>
2018-11-29 11:35:56 -0500

    add kotlin support, convert classes to kotlin

---
+++ src/main/java/com/tkj/garagedoorz/GarageDoorzApplication.kt
@@ -0,0 +1,11 @@
+package com.tkj.garagedoorz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GarageDoorzApplication
+
+fun main( args: Array<String> ) {
+	runApplication<GarageDoorzApplication>( *args )
+}
--- src/test/java/com/tkj/garagedoorz/GarageDoorzApplicationTests.kt
+++ src/test/java/com/tkj/garagedoorz/GarageDoorzApplicationTests.kt
@@ -5,12 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith( SpringRunner.class )
+@RunWith( SpringRunner::class )
 @SpringBootTest
-public class GarageDoorzApplicationTests {
+class GarageDoorzApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	fun contextLoads() {
 	}
 
 }
