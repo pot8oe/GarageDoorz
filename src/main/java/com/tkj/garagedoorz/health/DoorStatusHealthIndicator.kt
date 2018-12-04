5f7bc62885624706320da4925b55844cadd1ca16
Daniel Frey <dmfrey@gmail.com>
2018-12-04 08:34:30 -0500

    add @Component annotation to health indicator

--- src/main/java/com/tkj/garagedoorz/health/DoorStatusHealthIndicator.kt
+++ src/main/java/com/tkj/garagedoorz/health/DoorStatusHealthIndicator.kt
@@ -3,11 +3,13 @@ package com.tkj.garagedoorz.health
 import com.tkj.garagedoorz.domain.HwController
 import org.springframework.boot.actuate.health.AbstractHealthIndicator
 import org.springframework.boot.actuate.health.Health.Builder
+import org.springframework.stereotype.Component
 
 /**
  * HealthIndicator that reports garage door statuses.
  * @author Thomas G. Kenny Jr
  */
+@Component
 class DoorStatusHealthIndicator( private val hwController: HwController ) : AbstractHealthIndicator() {
 
     @Throws( Exception::class )
